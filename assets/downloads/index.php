5677826b4e34191e6841838d5a9fdbc88a347bb7
suxiang198 <suxiang@mail.ustc.edu.cn>
2017-09-20 17:46:14 +0800

    gForce V4.1.20 release

--- assets/downloads/index.php
+++ assets/downloads/index.php
@@ -1,2 +1,2 @@
-{"version_R3":"4.1.17","address_R3":"https://oymotion.github.io/assets/downloads/gForceAP_V4_1_17_20170919101435.bin",
+{"version_R3":"4.1.20","address_R3":"https://oymotion.github.io/assets/downloads/gForceAP_V4_1_20_20170920163024.bin",
 "version_R2":"v0.0-40","address_R2":"https://oymotion.github.io/assets/downloads/gForceAP_V2_20170621.bin"}
