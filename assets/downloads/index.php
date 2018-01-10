4a91b57a8963d83eddf240e2582dcc37de3ca87d
suxiang198 <suxiang@mail.ustc.edu.cn>
2018-01-10 13:39:04 +0800

    Deployed ba33e2b with MkDocs version: 0.17.1

--- assets/downloads/index.php
+++ assets/downloads/index.php
@@ -1,3 +1,3 @@
 {"version_R3":"","address_R3":"https://oymotion.github.io/assets/downloads/gForceAP_V4_1_54_20171105170458.bin",
-"version_R2":"v0.0-40","address_R2":"https://oymotion.github.io/assets/downloads/gForceAP_V2_20170621.bin",
+"version_R2":"","address_R2":"https://oymotion.github.io/assets/downloads/gForceAP_V2_20170621.bin",
 "hmm_V4":"","address_hmm_V4":"https://oymotion.github.io/assets/downloads/hmm_V4_1_20_20170920163024.bin"}
